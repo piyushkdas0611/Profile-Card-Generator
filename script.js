d43f8f62cca2f860802cbcd5d50449a4dcd73908
Piyush K. Das <piyush.k.das25@gmail.com>
2023-10-02 21:00:26 +0530

    added comments to follow while comtributing

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ generate.addEventListener("click", (e) => {
     e.preventDefault();
     generateCard();
 })
+
 function generateCard() {
     
     const imageSrc = document.getElementById("image").value;
@@ -15,7 +16,8 @@ function generateCard() {
     const email = document.getElementById("email").value;
 
     const card = document.getElementById("output");
-  
+    // Add your card here
+    // e.g const card1 = document.getElementById("output1");
     const cardHTML = `
     <div class="card">
         <img src="${imageSrc}" alt="image" class="profile">
@@ -30,7 +32,28 @@ function generateCard() {
         <button class="btn2" id="copy">Copy embedded link</button>
     </div>
     `;
+    // Create your own card here
+    // e.g: const cardHTML1 = `
+    // <div class="card">
+    //     <img src="${imageSrc}" alt="image" class="profile">
+    //     <h2>${name}</h2>
+    //     <p>${bio}</p>
+    //     <div class="links">
+    //         <a href="${twitterLink}" target="_blank"><img src="https://img.icons8.com/color/344/twitter-circled--v1.png"></a>
+    //         <a href="${githubLink}" target="_blank"><img src="https://img.icons8.com/color/344/github--v1.png"></a>
+    //         <a href="${linkedinLink}" target="_blank"><img src="https://img.icons8.com/color/344/linkedin-circled--v5.png"></a>
+    //     </div>
+    //     <a href="mailto:${email}"><button class="btn2">Contact me</button></a>
+    //     <button class="btn2" id="copy">Copy embedded link</button>
+    // </div>
+    // `
     card.innerHTML = cardHTML;
+
+    // Write the html of the card to DOM
+    // e.g card1.innerHTML = cardHTML1;
+    
+    //Create a copyHTML variable and store the HTML code without the copy embedded link button
+
     const copyHTML = `
     <div class="card">
         <img src="${imageSrc}" alt="image" class="profile">
@@ -44,6 +67,21 @@ function generateCard() {
         <a href="mailto:${email}"><button class="btn2">Contact me</button></a>
     </div>
     `;
+    
+    // e.g const copyHTML1 = `
+    // <div class="card">
+    //     <img src="${imageSrc}" alt="image" class="profile">
+    //     <h2>${name}</h2>
+    //     <p>${bio}</p>
+    //     <div class="links">
+    //         <a href="${twitterLink}" target="_blank"><img src="https://img.icons8.com/color/344/twitter-circled--v1.png"></a>
+    //         <a href="${githubLink}" target="_blank"><img src="https://img.icons8.com/color/344/github--v1.png"></a>
+    //         <a href="${linkedinLink}" target="_blank"><img src="https://img.icons8.com/color/344/linkedin-circled--v5.png"></a>
+    //     </div>
+    //     <a href="mailto:${email}"><button class="btn2">Contact me</button></a>
+    // </div>
+    // `;
+
     const copyButton = document.getElementById("copy");
     copyButton.addEventListener("click", function () {
         // Create a textarea element to hold the card HTML
